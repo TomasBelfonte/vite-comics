e24615540729ad3f9cf52557518f2e2ced6a1e70
TomasBelfonte <tomas.belfonte@gmail.com>
2022-12-04 17:48:28 +0100

    Add Nav Bar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
-<script setup>
-
-</script>
-
 <template>
-  
+  <TheNav></TheNav>
+  <h1>testo di prova</h1>
+  <h1>sono veramente felice quando le cose funzionano</h1>
+
 </template>
 
+<script>
+import TheNav from './TheNav.vue';
+
+export default {
+  components: { TheNav }
+};
+</script>
+
 <style lang="scss">
-  @use "./styles/general.scss"
+  @use "./style/general.scss";
+  @use "./style/partials/utilities.scss";
+  @use "./style/partials/variables.scss";
+
+  body {    
+    width: 1200px;
+    margin: 0 auto;
+    
+}
 
 </style>
---
+++ src/TheNav.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="d-flex">
+        <img class="logo" src="/dc-logo.png" alt="">
+        <ul class="nav nav-pills justify-content-center flex-grow-1">
+            <li class="nav-item" v-for="button in navButtons">
+                <a class="nav-link" href="#">{{button}}</a>
+            </li>
+            
+        </ul>
+        
+    </div>
+</template>
+
+
+<script>
+import { Button } from 'bootstrap';
+
+export default {
+    data () {
+        return {
+            navButtons: [
+            "CHACACTHERS",
+            "COMICS",
+            "MOVIES",
+            "TV",
+            "GAMES",
+            "COLLECTIBLES",
+            "VIDEOS",
+            "FANS",
+            "NEWS",
+            "SHOP"
+            ]
+        };
+    },
+    
+};
+</script>
+
+
+<style scoped>
+    .logo {
+        width: 40px;
+        height: 40px;
+    }
+
+    
+</style>
